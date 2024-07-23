fdcc00da3f83219122a6bf49d53cfa24fcc4d603
crish999 <145403536+crish999@users.noreply.github.com>
2024-07-22 20:36:15 -0500

    solucionado desempeno

--- src/controllers/evaluacionController.js
+++ src/controllers/evaluacionController.js
@@ -2,13 +2,29 @@ const Evaluacion = require('../models/evaluacionm');
 
 // Crear una evaluación
 exports.createEvaluacion = async (req, res) => {
+  const { id_area, id_empleado, id_desempeno } = req.body;
+
   try {
+    // Verificar si ya existe una evaluación con el mismo id_area, id_empleado y id_desempeno
+    const existingEvaluation = await Evaluacion.findOne({
+      where: {
+        id_area: id_area,
+        id_empleado: id_empleado,
+      }
+    });
+
+    if (existingEvaluation) {
+      return res.status(400).json({ message: 'Solo se puede agregar una única evaluación por área y empleado.' });
+    }
+
+    // Si no existe, proceder a crear la nueva evaluación
     const nuevaEvaluacion = await Evaluacion.create(req.body);
     res.status(201).json(nuevaEvaluacion);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
 
 // Obtener todas las evaluaciones
 exports.getEvaluaciones = async (req, res) => {
@@ -43,19 +59,22 @@ exports.getEvaluacionById = async (req, res) => {
 // Actualizar una evaluación por ID de área, ID de empleado y ID de desempeño
 exports.actualizarEvaluacion = async (req, res) => {
   try {
-    const [updated] = await Evaluacion.update(req.body, {
+    const { id_area, id_empleado } = req.params;
+    const { id_desempeno } = req.body;
+
+    // Actualiza la evaluación con el nuevo id_desempeno
+    const [updated] = await Evaluacion.update({ id_desempeno }, {
       where: {
-        id_area: req.params.id_area,
-        id_empleado: req.params.id_empleado,
-        id_desempeno: req.params.id_desempeno
+        id_area,
+        id_empleado
       }
     });
+
     if (updated) {
       const updatedEvaluacion = await Evaluacion.findOne({
         where: {
-          id_area: req.params.id_area,
-          id_empleado: req.params.id_empleado,
-          id_desempeno: req.params.id_desempeno
+          id_area,
+          id_empleado
         }
       });
       res.status(200).json(updatedEvaluacion);
@@ -66,6 +85,7 @@ exports.actualizarEvaluacion = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
 
 // Eliminar una evaluación por ID de área, ID de empleado y ID de desempeño
 exports.deleteEvaluacion = async (req, res) => {
